82a152d8660bc7c3de48f6b65fe885d43161b295
Jason M. Nugent <jasonmnugent@gmail.com>
2023-03-22 17:34:06 -0400

    add intermediate python project

---
+++ intermediate_python/membership_processor.py
@@ -0,0 +1,28 @@
+"""
+a club has a membership sign-up form where the data arrives as strings.
+Change the data types to fix it.
+"""
+
+name_box = "Alice Bentall"
+age_box = "20"
+uni_box = ""
+subs_box = "1.99"
+mkt_box = "0"
+
+# using a bool on a string will check if there is string is empty or not
+name_entered = bool(name_box)
+
+# if a name exists, if statement will run
+if name_entered:
+    name = name_box
+else:
+    name = "Unknown"
+
+age = int(age_box)
+student = bool(uni_box)
+subscription = float(subs_box)
+marketing = bool(int(mkt_box))
+
+
+profile = name + ', ' + str(age)
+print(profile)
